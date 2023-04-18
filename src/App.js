4de1d5df9e12fc73733d43c3a8df7abfbcc3ca0f
laura-williams-1 <laurawilliams@pursuit.org>
2023-04-17 22:13:55 -0400

    installed react-router-dom and wrapped browser router in index.js file

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import "./App.css";
+import Nav from "./Components/Nav";
+import Transaction from "./Components/transaction";
+import Index from "./Components/Index";
 
 function App() {
   return <div className="App"></div>;
