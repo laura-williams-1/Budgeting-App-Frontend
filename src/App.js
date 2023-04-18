21c0177752bb07d98cd32399c584d6b1f16f5547
laura-williams-1 <laurawilliams@pursuit.org>
2023-04-17 22:22:31 -0400

    imported components and added routes in app.js

--- src/App.js
+++ src/App.js
@@ -2,9 +2,18 @@ import "./App.css";
 import Nav from "./Components/Nav";
 import Transaction from "./Components/transaction";
 import Index from "./Components/Index";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/:transaction" element={<Transaction />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
