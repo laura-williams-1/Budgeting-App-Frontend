118fce18ebd2d69d5c4d2389c23bc07d2aee6fb4
laura-williams-1 <laurawilliams@pursuit.org>
2023-04-18 21:26:22 -0400

    added header, bank total, and amount to index page, index is now complete

--- src/Components/Transactions.js
+++ src/Components/Transactions.js
@@ -1,16 +1,25 @@
 const Transactions = ({ Transactions }) => {
+  const totalAmount = Transactions.reduce(
+    (total, transaction) => total + transaction.amount,
+    0
+  );
+
   return (
-    <tbody>
-      {Transactions.map((transaction) => {
-        return (
-          <tr key={transaction.id}>
-            <td>{transaction.date}</td>
-            <td>{transaction.from}</td>
-            <td>{transaction.category}</td>
-          </tr>
-        );
-      })}
-    </tbody>
+    <>
+      <h1>Bank Account Total {totalAmount}</h1>
+      <tbody>
+        {Transactions.map((transaction) => {
+          return (
+            <tr key={transaction.id}>
+              <td>{transaction.date}</td>
+              <td>{transaction.from}</td>
+              <td>{transaction.amount}</td>
+              <td>{transaction.category}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </>
   );
 };
 export default Transactions;
