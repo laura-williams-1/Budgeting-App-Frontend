dbf2733b62785e8c3d70570d64e1ef1ddb4c81db
laura-williams-1 <laurawilliams@pursuit.org>
2023-04-19 09:39:27 -0400

    axios call for index completed

--- src/Components/Transaction.js
+++ src/Components/Transaction.js
@@ -1,7 +1,23 @@
-// const Transaction = () => {
-//   return (
-//     <div>
-//       <h1>Hello</h1>
-//     </div>
-//   );
-// };
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Link, useNavigate, useParams } from "react-router-dom";
+const API = process.env.REACT_APP_API_URL;
+
+const Transaction = () => {
+  const [transaction, setTransaction] = useState([]);
+  let { id } = useParams();
+  let navigate = useNavigate();
+  useEffect(() => {
+    axios
+      .get(`${API}/transactions/${id}`)
+      .then((res) => setTransactions(res.data))
+      .catch((e) => console.log(e));
+  }, [id, navigate]);
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
+};
+
+export default Transaction;
