95bd4cf39eb53c2618921a910b70bf0613cd885c
laura-williams-1 <laurawilliams@pursuit.org>
2023-04-19 09:43:41 -0400

    updated index APIcall & created function to handle deleted transactions

--- src/Components/Transaction.js
+++ src/Components/Transaction.js
@@ -11,8 +11,17 @@ const Transaction = () => {
     axios
       .get(`${API}/transactions/${id}`)
       .then((res) => setTransactions(res.data))
-      .catch((e) => console.log(e));
+      .catch(() => navigate("/not-found"));
   }, [id, navigate]);
+
+  const delteHandler = () => {
+    axios
+      .delete(`${API}/transactions/${id}`)
+      .then(() => {
+        navigate(`/transactions`);
+      })
+      .catch((err) => console.log(err));
+  };
   return (
     <div>
       <h1>Hello</h1>
